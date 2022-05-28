8bd920af028b2f98d0029846bbc21d3798fb7457
Joanna Chądzyńska <jmalek87@gmail.com>
2022-05-28 22:03:46 +0200

    add click outside hoot

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,6 +1,34 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from './store';
+import { RefObject, useCallback, useEffect, useState } from "react";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import type { RootState, AppDispatch } from "./store";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useOnClickOutside = <T extends HTMLElement = HTMLElement>(
+    ref: RefObject<T>,
+    handler: (event: Event) => void
+): void => {
+    useEffect(
+        () => {
+            const listener = (event: Event) => {
+                // Do nothing if clicking ref's element or descendent elements
+                const el = ref?.current;
+                if (!el || el.contains((event?.target as Node) || null)) {
+                    return;
+                }
+                handler(event); // Call the handler only if the click is outside of the element passed.
+            };
+
+            document.addEventListener("mousedown", listener);
+            document.addEventListener("touchstart", listener);
+
+            return () => {
+                document.removeEventListener("mousedown", listener);
+                document.removeEventListener("touchstart", listener);
+            };
+        },
+        [ref, handler] // Reload only if ref or handler changes
+    );
+};
