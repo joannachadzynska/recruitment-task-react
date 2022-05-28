f397a9534259b2534bdb40c3e2a0d3cabf01f489
Joanna Chądzyńska <jmalek87@gmail.com>
2022-05-28 22:03:31 +0200

    update reducer

--- src/store/reducers/githubUsersReducer.ts
+++ src/store/reducers/githubUsersReducer.ts
@@ -1,12 +1,12 @@
-import { Action, AnyAction } from "redux";
+import { AnyAction } from "redux";
 import { Commit } from "../../models/commit.model";
 import { Repository } from "../../models/repo.model";
-import { User, UserSearchResponse } from "../../models/user.model";
+import { UserDetails, UserSearchResponse } from "../../models/user.model";
 import { GithubUsersActions } from "../action-creators/githubUsers.actions";
 
 interface State {
     users: UserSearchResponse | null;
-    user: User | null;
+    user: UserDetails | null;
     repos: Repository[];
     commits: Commit[];
     loading: boolean;
@@ -46,6 +46,72 @@ export const githubUsersReducer = (
                 ...state,
                 loading: false,
                 users: null,
+                error: action.payload,
+            };
+
+        case GithubUsersActions.GET_USER_BY_LOGIN_NAME:
+            return {
+                ...state,
+                user: null,
+                loading: true,
+            };
+
+        case GithubUsersActions.GET_USER_BY_LOGIN_NAME_SUCCESS:
+            return {
+                ...state,
+                user: action.payload,
+                loading: false,
+            };
+
+        case GithubUsersActions.GET_USER_BY_LOGIN_NAME_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                user: null,
+                error: action.payload,
+            };
+
+        case GithubUsersActions.GET_USER_REPOS:
+            return {
+                ...state,
+                repos: [],
+                loading: true,
+            };
+
+        case GithubUsersActions.GET_USER_REPOS_SUCCESS:
+            return {
+                ...state,
+                repos: action.payload,
+                loading: false,
+            };
+
+        case GithubUsersActions.GET_USER_REPOS_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                repos: [],
+                error: action.payload,
+            };
+
+        case GithubUsersActions.GET_COMMITS:
+            return {
+                ...state,
+                commits: [],
+                loading: true,
+            };
+
+        case GithubUsersActions.GET_COMMITS_SUCCESS:
+            return {
+                ...state,
+                commits: action.payload,
+                loading: false,
+            };
+
+        case GithubUsersActions.GET_COMMITS_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                commits: [],
                 error: action.payload,
             };
 
