dccadfb546e19eb5c721124eb31d520823953a50
Joanna Chądzyńska <jmalek87@gmail.com>
2022-06-02 08:15:22 +0200

    remove home page

--- src/pages/Home/Home.tsx
+++
@@ -1,8 +0,0 @@
-import "./Home.style.scss";
-interface HomePageProps {}
-
-const HomePage: React.FC<HomePageProps> = () => {
-    return <h2>Landing (Public: anyone can access this page)</h2>;
-};
-
-export default HomePage;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,4 +2,3 @@ export { About } from "./About";
 export { Factorial } from "./Factorial";
 export { GithubUser } from "./GithubUser";
 export { GithubUserSearch } from "./GithubUserSearch";
-export { Home } from "./Home";
